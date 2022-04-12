b98104c5a48ab5e5e8beb1706615d3a621213f32
Jeyi Adole <erthnice@gmail.com>
2022-04-12 15:36:10 +0100

    creating actions and reducers using slice

--- 2-redux-starter/src/store/bugs.js
+++ 2-redux-starter/src/store/bugs.js
@@ -1,45 +1,34 @@
-import { createAction, createReducer } from '@reduxjs/toolkit';
+import { createSlice } from '@reduxjs/toolkit';
 
-// Creates your actions, obviously
-export const addBug = createAction('addBug');
-export const resolveBug = createAction('resolveBug');
-export const removeBug = createAction('removeBug');
-
-// Reducers
 let lastId = 0;
 
-// Recall to export as default, else the store is null
-// First parameter = initial state.
-export default createReducer([], {
-  // key: value
-  // actions: functions (event => event handler)
+// Creates the actions and the reducers
+const slice = createSlice({
+  name: 'bugs',
+  initialState: [],
+  reducers: {
+    // An object that maps actions to action handlers
 
-  //   the state parameter has been renamed to bugs
+    // addBug is no longer an object but the name of the property which will create action types and reducers
+    addBug: (bugs, action) => {
+      bugs.push({
+        id: ++lastId,
+        description: action.payload.description,
+        resolved: false,
+      });
+    },
 
-  //   Type of action must equal name of reducer
+    resolveBug: (bugs, action) => {
+      const index = bugs.findIndex((bug) => bug.id === action.payload.id);
+      bugs[index] = true;
+    },
 
-  [addBug.type]: (bugs, action) => {
-    bugs.push({
-      id: ++lastId,
-      description: action.payload.description,
-      resolved: false,
-    });
-  },
-
-  [resolveBug.type]: (bugs, action) => {
-    const index = bugs.findIndex((bug) => bug.id === action.payload.id);
-    bugs[index] = true;
-  },
-
-  [removeBug.type]: (bugs, action) => {
-    bugs.filter((bug) => bug.id === action.payload.id);
+    removeBug: (bugs, action) => {
+      bugs.filter((bug) => bug.id === action.payload.id);
+    },
   },
 });
+// console.log(slice);
 
-// DUCK'S PATTERN RULES
-/*
- - Reducer must be a default export
- - Export Individual action creators
- - No need to export action types 
-
-*/
+export const { addBug, resolveBug, removeBug } = slice.actions;
+export default slice.reducer;
