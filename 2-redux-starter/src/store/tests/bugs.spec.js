62adbbf403617a38671ba757d1d513c377fd5cfd
Jeyi Adole <erthnice@gmail.com>
2022-04-18 20:48:49 +0100

    Social tests unfinished

--- 2-redux-starter/src/store/tests/bugs.spec.js
+++ 2-redux-starter/src/store/tests/bugs.spec.js
@@ -1,21 +1,24 @@
 import { bugAdded, addBug } from '../bugs';
 import * as actions from '../api';
+import configureStore from '../configureStore';
 
 describe('bugsSlice', () => {
-  describe('action creators', () => {
-    it('addBug', () => {
-      const bugDescription = { description: 'a' };
-      const result = bugAdded(bugDescription);
-      const expected = {
-        type: actions.apiCallBegan.type,
-        payload: {
-          url: '/bugs',
-          method: 'post',
-          data: bugDescription,
-          onSuccess: addBug.type,
-        },
-      };
-      expect(result).toEqual(expected);
-    });
+  it('should handle the addBug action', async () => {
+    const store = configureStore();
+    const bugObject = { description: 'a' };
+    const x = store.dispatch(addBug(bugObject));
+    console.log(store.getState());
+
+    //   const result = bugAdded(bugDescription);
+    //   const expected = {
+    //     type: actions.apiCallBegan.type,
+    //     payload: {
+    //       url: '/bugs',
+    //       method: 'post',
+    //       data: bugDescription,
+    //       onSuccess: addBug.type,
+    //     },
+    //   };
+    //   expect(result).toEqual(expected);
   });
 });
