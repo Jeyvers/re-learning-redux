98ef01f6414b412f31518c19daf0db204aeafecc
Jeyi Adole <erthnice@gmail.com>
2022-04-24 19:55:09 +0100

    thunk with axios

--- 4-redux-toolkit-w-johnsmilga/starter/src/features/cart/cartSlice.js
+++ 4-redux-toolkit-w-johnsmilga/starter/src/features/cart/cartSlice.js
@@ -1,4 +1,4 @@
-import cartItems from '../../cartItems';
+import axios from 'axios';
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 
 const url = 'https://course-api.com/react-useReducer-cart-project';
@@ -10,10 +10,11 @@ const initialState = {
   isLoading: true,
 };
 
-export const getCartItems = createAsyncThunk('cart/getCartItems', () => {
-  return fetch(url)
-    .then((res) => res.json())
-    .catch((err) => console.log(err));
+export const getCartItems = createAsyncThunk('cart/getCartItems', async () => {
+  try {
+    const res = await axios(url);
+    return res.data;
+  } catch (error) {}
 });
 
 const cartSlice = createSlice({
