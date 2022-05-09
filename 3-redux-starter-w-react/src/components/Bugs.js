8ec7bbad019fd97c037fdb959f4f015471dd44d3
Jeyi Adole <erthnice@gmail.com>
2022-05-09 12:53:47 +0100

    Changes to class components

--- 3-redux-starter-w-react/src/components/Bugs.js
+++ 3-redux-starter-w-react/src/components/Bugs.js
@@ -1,7 +1,4 @@
-import React from 'react';
-import { useState } from 'react';
-import { useContext } from 'react';
-import { useEffect } from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import {
   bugResolved,
@@ -10,31 +7,37 @@ import {
   resolveBug,
 } from '../store/bugs';
 
-const Bugs = (props) => {
-  useEffect(() => {
-    props.loadBugs();
-  }, []);
+class Bugs extends Component {
+  componentDidMount() {
+    this.props.loadBugs();
+  }
 
-  return (
-    <ul>
-      {props.bugs.map((bug) => (
-        <li key={bug.id}>
-          {bug.description}{' '}
-          <button onClick={() => props.resolveBug(bug.id)}>Resolve</button>
-        </li>
-      ))}
-    </ul>
-  );
-};
+  render() {
+    return (
+      <ul>
+        {this.props.bugs.map((bug) => (
+          <li key={bug.id}>
+            {bug.description}
+            <button onClick={() => this.state.dispatch(bugResolved(bug.id))}>
+              Resolve
+            </button>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
 
-// interest: state.entitties.bugs.list
+// interest: state.entities.bugs.list, takes the state and returns an object.
 const mapStateToProps = (state) => ({ bugs: getUnresolvedBugs(state) });
 
+// for dispatching actions
 const mapDispatchToProps = (dispatch) => ({
   loadBugs: () => dispatch(loadBugs()),
   resolveBug: (id) => dispatch(bugResolved(id)),
 });
 
-// Container
-// Presentation (Bugs)
+// Container - the component that is returned from the expression
+// Presentation component - (Bugs)
+// Connect is a higher order function.
 export default connect(mapStateToProps, mapDispatchToProps)(Bugs);
--- 4-redux-toolkit-w-johnsmilga/starter/src/store.js
+++ 4-redux-toolkit-w-johnsmilga/starter/src/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import './features/cart/cartSlice';
 import cartReducer from './features/cart/cartSlice';
-import modalReducer from './features/modal/modalSlice';
+
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
-    modal: modalReducer,
   },
 });
